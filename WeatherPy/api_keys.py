69ebea5240c91cf9d59764d551ce1864b931f863
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-07-03 15:00:54 -0700

    complete project

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+weather_api_key = "c72e960951e439ad9be0e612600d2981"
+
+
+# In[ ]:
+
+
+
+
---
+++ g_api_key.py
@@ -0,0 +1,1 @@
+g_key = "AIzaSyBL3qNHpdXiSozSfa-1yMuCQDscgOQTjSk"
